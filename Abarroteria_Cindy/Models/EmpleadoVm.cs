a4907eb1d7d6d6fe5cef250dfb67ecef75a96bee
LUIS HERNANDEZ <luigihr2001@gmail.com>
2024-03-16 15:01:04 -0600

    Modifique las tablas cliente y empleado y ya esta creado el crud de empleados

--- Abarroteria_Cindy/Models/EmpleadoVm.cs
+++ Abarroteria_Cindy/Models/EmpleadoVm.cs
@@ -1,4 +1,5 @@
 ﻿using Abarroteria_Cindy.Data.Entidades;
+using System.Text.RegularExpressions;
 
 namespace Abarroteria_Cindy.Models
 {
@@ -11,10 +12,78 @@ namespace Abarroteria_Cindy.Models
         public DateTime Fecha_Nacimiento { get; set; }
         public int Telefono { get; set; }
         public string? DNI { get; set; }
-        public bool Sexo { get; set; }
+        public string? Sexo { get; set; }
         public string? Correo { get; set; }
         public string? Contraseña { get; set; }
+        public string? Contraseña2 { get; set; }
         public RolVm? Rol { get; set; }
         public Guid RolId { set; get; }
+        public bool Validacion()
+        {
+            if (string.IsNullOrEmpty(Nombre))
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(Apellido))
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(Direccion))
+            {
+                return false;
+            }
+            if (Fecha_Nacimiento != null)
+            {
+                DateTime fechaNacimiento;
+                if (DateTime.TryParse(Fecha_Nacimiento.ToString(), out fechaNacimiento))
+                {
+                    // Calcular la edad actual
+                    int edad = DateTime.Today.Year - fechaNacimiento.Year;
+                    if (DateTime.Today < fechaNacimiento.AddYears(edad))
+                    {
+                        edad--;
+                    }
+
+                    // Comprobar si la edad está dentro del rango
+                    if (edad < 15 || edad > 50)
+                    {
+                        // La edad no está dentro del rango permitido
+                        return false;
+                    }
+                }
+                else
+                {
+                    // La fecha de nacimiento introducida no es válida
+                    return false;
+                }
+            }
+
+            if (Telefono.ToString().Length != 8 || Telefono != null)
+            {
+                return false;
+            }
+            if (DNI.Length != 13 || DNI != null)
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(Sexo))
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(Correo) || !Regex.IsMatch(Correo, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(Contraseña2))
+            {
+                return false;
+            }
+            if (string.IsNullOrEmpty(Contraseña) || Contraseña != Contraseña2)
+            {
+                return false;
+            }
+
+            return true;
+        }
     }
 }
