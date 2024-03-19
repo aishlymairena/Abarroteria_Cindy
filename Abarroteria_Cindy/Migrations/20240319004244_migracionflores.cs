7694e7edb95e8cdf68bf88bf5da7f4393e70311b
Marvin-Flores16 <marvin.floresdaniel16@gmail.com>
2024-03-19 12:04:01 -0600

    Creacion de index Producto y Inventario

---
+++ Abarroteria_Cindy/Migrations/20240319004244_migracionflores.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abarroteria_Cindy.Migrations
+{
+    public partial class migracionflores : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
